6d735128d3ecf4702390936f105f9db96d19d164
SebasAKD <sebastian.minici@gmail.com>
2018-09-05 22:07:34 -0300

    Clase de colletions

--- src/clase1/genericos/MainClass.java
+++ src/clase1/genericos/MainClass.java
@@ -1,19 +1,25 @@
 package clase1.genericos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		CachePersona cp = new CachePersona();
 		CacheString cs = new CacheString();
 		
-		CacheGenerica<Persona> cgp = new CacheGenerica<>();
-		//CacheGenerica<String> cgs = new CacheGenerica<>();
+//		CacheGenerica<Persona> cgp = new CacheGenerica<>();
+//		//CacheGenerica<String> cgs = new CacheGenerica<>();
+//		
+//		cgp.guardar(new Persona());
+//		cgs.guardar("un string");
+//		
+//		cgp.imprimirContenido();
+//		cgs.imprimirContenido();
 		
-		cgp.guardar(new Persona());
-		cgs.guardar("un string");
 		
-		cgp.imprimirContenido();
-		cgs.imprimirContenido();
+		
 	}
 
 }
--- src/clase1/genericos/Persona.java
+++ src/clase1/genericos/Persona.java
@@ -1,6 +1,26 @@
 package clase1.genericos;
 
-public class Persona implements Saludable{
+public class Persona implements Saludable, Comparable<Persona> {
+
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		Persona p = (Persona) obj;
+		return p.getName().equals(this.getName());
+	}
+
+
 
 	@Override
 	public String toString() {
@@ -11,6 +31,19 @@ public class Persona implements Saludable{
 	public void imprimirPeso() {
 		System.out.println("80");
 		
+	}
+
+	@Override
+	public int compareTo(Persona o) {
+		System.out.println("usa compareTo");
+			return this.getName().compareTo(o.getName());
+			
+	}
+
+	@Override
+	public int hashCode() {
+		System.out.println("usa hashcode : " + this.getName().hashCode());
+		return this.getName().hashCode();
 	}
 	
 	
